e6435f1fdc46e3055566ce1c2eca51254b09cecc
Marcus J. Carey <marcuscarey@gmail.com>
2011-09-30 16:38:54 -0500

    Edited vSploit/vsploit_filestream.rb via GitHub

--- vSploit/vsploit_filestream.rb
+++ vSploit/vsploit_filestream.rb
@@ -92,9 +92,7 @@ class Metasploit3 < Msf::Auxiliary
 				data = rar
 			when "ZIP"
 				data = zip
-			#else
-			#	print_error("Please set FILE_TYPE to: ELF, EXE, RAR, or ZIP")
-			#	return
+
 		end
 
 		# Create TCP connection and send data
